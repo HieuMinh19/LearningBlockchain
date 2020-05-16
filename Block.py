9e6532113ec0f42326d03215d0a1f565a9645696
hieule <lehieuminh231@gmail.com>
2020-05-16 16:46:32 +0700

    proof_of_work

--- Block.py
+++ Block.py
@@ -1,6 +1,6 @@
 from hashlib import sha256
 class Block:
-    def __init__(self, index, previousHash, timestamp, transaction, hashData):
+    def __init__(self, index, previousHash, timestamp, transaction, hashData, nonce):
         """
         Constructor cho một `Block` class.
         :param index: Chỉ số ID duy nhất của một block.
@@ -12,8 +12,10 @@ class Block:
         self.timestamp = timestamp
         self.transaction = transaction
         self.hashData = hashData
-    
+        self.nonce = nonce
+
     def calculateHashForBlock(self):
-        data = (str(self.index) + self.previousHash + str(self.timestamp) + self.transaction).encode('utf-8')
+        data = (str(self.index) + self.previousHash + 
+        str(self.timestamp) + self.transaction + str(self.nonce)).encode('utf-8')
         self.hashData = sha256(data).hexdigest()
         return self.hashData
